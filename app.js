8526fcd024511f9cacd63098bfb219e7e4cfafce
joss-bleach <joss.bleach@outlook.com>
2020-08-03 17:36:16 +0100

    inital graphql setup

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 // Set up app and json parser middleware
 const app = express();
 app.use(express.json({ extended: false }));
+
+// GraphQL server setup
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Codecon", "Comiccon"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 // Assign port
 const PORT = process.env.PORT || 3000;
