cc95393787b9a58538aac7913b395f4298527335
joss-bleach <joss.bleach@outlook.com>
2020-08-04 13:03:10 +0100

    Add user

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 const connectDb = require("./config/db");
+const bcrypt = require("bcryptjs");
 
 // Mongoose Models
 const Event = require("./models/Event");
+const User = require("./models/User");
 
 // Set up app and json parser middleware
 const app = express();
@@ -26,6 +28,17 @@ app.use(
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String 
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         input EventInput {
             title: String!
             description: String!
@@ -39,6 +52,7 @@ app.use(
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -50,9 +64,6 @@ app.use(
       events: async () => {
         try {
           const events = await Event.find();
-          if (!events) {
-            console.log("No Events.");
-          }
           return events.map((event) => {
             return { ...event._doc };
           });
@@ -63,17 +74,46 @@ app.use(
       createEvent: async (args) => {
         try {
           const { title, description, price, date } = args.eventInput;
+
           const event = new Event({
             title: title,
             description: description,
             price: price,
             date: new Date(date),
+            creator: "5f294bfdcef84b0982bd11fd",
           });
+
           const newEvent = await event.save(event);
-          if (!newEvent) {
-            console.log("Error.");
-          }
+          const userEvent = await User.findById("5f294bfdcef84b0982bd11fd");
+          userEvent.createdEvents.push(newEvent);
+          const updateUser = await userEvent.save();
           return newEvent;
+        } catch (err) {
+          console.log(err);
+          throw err;
+        }
+      },
+      createUser: async (args) => {
+        try {
+          const { email, password } = args.userInput;
+
+          const checkUser = await User.findOne({ email: email });
+
+          if (checkUser) {
+            throw new Error("Email address already in use.");
+          }
+
+          const user = new User({
+            email: email,
+            password: password,
+          });
+
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(password, salt);
+
+          const newUser = await user.save(user);
+          const createdUser = { ...newUser._doc, password: null };
+          return createdUser;
         } catch (err) {
           console.log(err);
           throw err;
